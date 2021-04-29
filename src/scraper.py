1b4408d1c465efefd223499e0a9a6d198f47a126
Chasity Chen <chasitychen0128>
2021-04-29 12:50:47 +0800

    update class label

--- src/scraper.py
+++ src/scraper.py
@@ -20,8 +20,13 @@ def main():
 
     base_url = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid='
     # A list of teacher id whose comments we will fetch
-    tid = list(range(9, 2499999, 4000))
+    tid = [x for x in list(range(1000, 2000, 1)) if (x % 100 != 0 and (x % 1000 not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))]
     # [1302, 2224004, 1576103, 1051004, 2105994, 2291871, 1482580, 2454762, 1032165, 2291493, 1134872, 1889463, 919428, 2190976, 534980]
+    # used: 
+    #   (0, 2500000, 100), 
+    #   (1, 2500000，1000), 
+    #   (9, 2500000),
+    #   (5, 2500000)
 
     for t in tid:
         try: 
@@ -38,7 +43,7 @@ def main():
             difficulty = float(dom.find_all('div', {'class': 'FeedbackItem__FeedbackNumber-uof32n-1 kkESWs'})[1].text)
             
             # Get all the comments of this professor
-            comments_selector = dom.find('ul', {'class': 'RatingsList__RatingsUL-hn9one-1 kHITzZ'}).select('li')
+            comments_selector = dom.find('ul', {'class': 'RatingsList__RatingsUL-hn9one-0 cbdtns'}).select('li')
             quality_class = ['CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 kMhQxZ', 'CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 bUneqk', 'CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 fJKuZx']
             difficulty_class = ['CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 cDKJcc']
 
@@ -55,21 +60,31 @@ def main():
                         i += 1
                 d = float(comment.find('div', {'class': 'CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 cDKJcc'}).text)
                 vc = comment.find('div', {'class': 'Comments__StyledComments-dzzyvm-0 gRjWel'}).text
+
+                # Only get negative comments
+                #if q <= 2:
+                #    comments.append([q, d, vc])
+
+                # Get all comments
                 comments.append([q, d, vc])
-            
+
             # Each professor is stored as a dictionary in the following form:
             # {name: Adam Meyers, overall_score: 3.3, would_take_again: 0.67, difficulty: 3, comments: [a_list_of_comments]}
             # "comments" stores all the comments for this professor. Each comment is stored as a list in the form of [quality, difficulty, verbal_comment].
             prof = {'name': name, 'overall_score': overall_score, 'would_take_again': would_take_again, 'difficulty': difficulty, 'comments': comments}
             prof_list.append(prof)
+            print(str(t) + ' success')
         
         except: 
+            print(t)
             continue
+
+        
 
     print("Total number of professors: " + str(len(prof_list)))
     print("Total number of comments: " + str(sum([len(prof['comments']) for prof in prof_list])))
 
-    output_csv(prof_list, '../data/rmp_data5.csv')
+    output_csv(prof_list, '../data/rmp_data_neg4.csv')
     return prof_list
 
 if __name__ == '__main__':
